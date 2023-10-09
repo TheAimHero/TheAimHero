5b96a83cc593071506c16ff930141ed8dbb51038
TheAimHero <vsghodekar1@gmail.com>
2023-10-09 23:28:10 +0530

    [fix] Neovim fix gx textobject command

--- Neovim/.config/nvim/lua/plugins/various-textobjs.lua
+++ Neovim/.config/nvim/lua/plugins/various-textobjs.lua
@@ -8,26 +8,30 @@ return {
 		})
 
 		vim.keymap.set("n", "gx", function()
-			require("various-textobjs").url() -- select URL
-			local foundURL = vim.fn.mode():find("v") -- only switches to visual mode if found
-			local url
-			if foundURL then
-				vim.cmd.normal('"zy') -- retrieve URL with "z as intermediary
-				url = vim.fn.getreg("z")
+			-- select URL
+			require("various-textobjs").url()
 
-				local opener
-				if vim.fn.has("macunix") == 1 then
-					opener = "open"
-        elseif vim.fn.has("wsl") == 1 then
-					opener = "xdg-open "
-				elseif vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
-					opener = "start"
-				end
-				vim.cmd("silent ! " .. opener .. "'" .. url .. "'")
-			else
-				-- if not found in proximity, search whole buffer via urlview.nvim instead
-				vim.cmd.UrlView("buffer")
+			-- plugin only switches to visual mode when textobj found
+			local foundURL = vim.fn.mode():find("v")
+			if not foundURL then
+				return
 			end
-		end, { desc = "Smart URL Opener" })
+
+			-- retrieve URL with the z-register as intermediary
+			vim.cmd.normal({ '"zy', bang = true })
+			local url = vim.fn.getreg("z")
+
+			-- open with the OS-specific shell command
+			local opener
+			if vim.fn.has("macunix") == 1 then
+				opener = "open"
+			elseif vim.fn.has("linux") == 1 then
+				opener = "xdg-open"
+			elseif vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
+				opener = "start"
+			end
+			local openCommand = string.format("%s '%s' >/dev/null 2>&1", opener, url)
+			vim.fn.system(openCommand)
+		end, { desc = "URL Opener" })
 	end,
 }
