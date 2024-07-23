35c8cf382c8706f794b8e270f68b37bb801e28e6
TheAimHero <91832491+TheAimHero@users.noreply.github.com>
2024-07-24 01:09:46 +0530

    nix: add zsh exports

--- NixOs/app/zsh/zsh-exports.sh
+++ NixOs/app/zsh/zsh-exports.sh
@@ -7,3 +7,10 @@ VI_MODE_SET_CURSOR=true
 VI_MODE_ESC_INSERT="jk"
 VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
 
+export PATH="$HOME/.local/bin":$PATH
+export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME/go/bin:$PATH
+export PATH=$HOME/.miniconda/bin:$PATH
+export PATH=/usr/bin/python3:$PATH
+export GOPATH=$HOME/go:$PATH
+export VI_MODE_ESC_INSERT="jk"
